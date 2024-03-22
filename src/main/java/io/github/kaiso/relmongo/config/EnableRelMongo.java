8acb36613946781104592eadec46b80139ec22f7
i-telligence-af <alexander.fuckert@i-telligence.de>
2024-03-22 10:29:48 +0100

    .

--- src/main/java/io/github/kaiso/relmongo/config/EnableRelMongo.java
+++ src/main/java/io/github/kaiso/relmongo/config/EnableRelMongo.java
@@ -36,7 +36,7 @@ import java.lang.annotation.*;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = { ElementType.TYPE, ElementType.METHOD })
+@Target(value = { ElementType.TYPE })
 @Import(value = RelMongoProcessorRegistrar.class)
 public @interface EnableRelMongo {
     
